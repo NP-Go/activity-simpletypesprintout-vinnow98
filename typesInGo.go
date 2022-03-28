6bbe6d225eff8c6a5d4c51c0d7cefaa8f19e9eae
vinnow98 <evinwoon@gmail.com>
2022-03-28 16:53:31 +0800

    added all the types

--- typesInGo.go
+++ typesInGo.go
@@ -4,6 +4,17 @@ package main
 
 func tellMeTypes() {
 	//insert code here to print out types of variables
+	text:= "the following is the account information
+	firstName:= "Luke"
+	lastName:= "Skywalker"	
+	age:= 20
+	weight:= 73.0
+	height := 1.72
+	remainingCredit:= 123.55
+	accountName:= "admin"
+	accountPassword:= "password"
+	subscribedUser:= True
+	
 }
 
 func main() {
