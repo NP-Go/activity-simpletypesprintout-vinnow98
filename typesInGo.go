ad699791bdaf7592176ec1ae6bf4fdbd63436ac7
vinnow98 <evinwoon@gmail.com>
2022-03-29 09:36:30 +0800

    completed activity, How to make it less clunky?

--- typesInGo.go
+++ typesInGo.go
@@ -1,22 +1,19 @@
 package main
 
-//Insert variables declarations here based on activity
-
-func tellMeTypes() {
-	//insert code here to print out types of variables
-	text:= "the following is the account information
-	firstName:= "Luke"
-	lastName:= "Skywalker"	
-	age:= 20
-	weight:= 73.0
-	height := 1.72
-	remainingCredit:= 123.55
-	accountName:= "admin"
-	accountPassword:= "password"
-	subscribedUser:= True
-	
-}
+import "fmt"
 
 func main() {
-	tellMeTypes()
+	text := "the following is the account information"
+	firstName := "Luke"
+	lastName := "Skywalker"
+	age := 20
+	weight := 73.0
+	height := 1.72
+	remainingCredit := 123.55
+	accountName := "admin"
+	accountPassword := "password"
+	subscribedUser := true
+
+	fmt.Printf("%v \n%v \n%v \n%v \n%v \n%v \n%v \n%v \n%v \n%v", text, firstName, lastName, age, weight, height, remainingCredit, accountName,
+		accountPassword, subscribedUser)
 }
